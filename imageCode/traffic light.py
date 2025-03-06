8764794a37bd0b72504c124ff8f05e2b5644270d
shooeys <133713818+shooeys@users.noreply.github.com>
2025-03-06 14:06:16 -0500

    Create traffic light.py

---
+++ imageCode/traffic light.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+def draw_minimalist_stoplight():
+    fig, ax = plt.subplots()
+    
+    # Draw the stoplight pole
+    pole = plt.Rectangle((0.45, 0.1), 0.1, 0.8, edgecolor='black', facecolor='none', linewidth=2)
+    ax.add_patch(pole)
+    
+    # Draw the stoplight lights
+    light_red = plt.Circle((0.5, 0.8), 0.05, edgecolor='black', facecolor='none', linewidth=2)
+    light_yellow = plt.Circle((0.5, 0.6), 0.05, edgecolor='black', facecolor='none', linewidth=2)
+    light_green = plt.Circle((0.5, 0.4), 0.05, edgecolor='black', facecolor='none', linewidth=2)
+    ax.add_patch(light_red)
+    ax.add_patch(light_yellow)
+    ax.add_patch(light_green)
+    
+    # Set limits and aspect
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.set_aspect('equal')
+    ax.axis('off')  # Turn off the axis
+    
+    plt.show()
+    return fig
+
+# Generate and show the minimalist stoplight illustration
+figure_stoplight = draw_minimalist_stoplight()
